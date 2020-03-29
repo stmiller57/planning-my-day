4a19a396fe4afb7df900dcbbeca5e179f51a4e28
Reformed Scribe <stmiller57@gmail.com>
2020-03-29 17:12:51 -0400

    Added comments and pseudo code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # planning-my-day
+
+This assignment called for the use of HTML, CSS and jQuery to generate a simple day planner. 
+
+I started by building a row of static HTML elements with headers for each column. I then worked on the JavaScript to inject a series of rows to give time blocks from 9 am to 5 pm. 
+
+I also used moment.JS to place the current time into the header. 
+
+Finally, I tweaked the color scheme to feature mostly green, white and gray. 
+
+## Trouble spots
+I was unable to complete several aspects of the assignment, though I did try to pseudo-code it. 
+
+I couldn't crack local storage for a second straight week. This has been one of my biggest stumbling blocks in the first six weeks of bootcamp. 
+
+I also wasn't able to take the time data and change the colors of the event blocks to reflect whether it was past, present or future. 
+
--- main.js
+++ main.js
@@ -21,8 +21,21 @@ $(document).ready(function () {
         time.innerhtml;
         event.attr("class", "col-sm-8 eventHead");
         event.html("<textarea></textarea>");
-        save.attr("class", "col-sm-2 btn btn-secondary btn-lg saveBtn");
+        save.attr("class", "col-sm-2 btn btn-dark saveBtn");
         save.html("<input type=button value=save>");
         nextRow.attr("class", "row hour");
     };
 });
+
+// This is where I ran into trouble. I understand what I still need to do but haven't been able to figure out the code to do it.
+
+// Here is what is missing that I couldn’t code.
+
+// 1. On-click functionality for the save buttons. I believe I should do something like what’s below:
+// $('saveBtn').click(function () {
+// });
+
+// 2. A way to save the events to local storage. I know I’m supposed to use setItem and getItem, but I don’t understand the syntax yet. I also struggled with this in my Week 4 homework. I’m hoping a tutor will help me with it this week. 
+
+// 3. A way to change the colors based on the time of day. I know I need to attach the classes from the CSS file (.past, .present, .future), but I’m unable to generate the code that will change the event column based on time of day. I understand we are supposed to use moment.JS but I was unable to go any further than this. 
+
